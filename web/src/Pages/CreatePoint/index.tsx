763c3ade95abade281eb2b8d17c155ab93b104b6
Leonardo Carneiro <leocarneiroo@gmail.com>
2022-08-10 22:42:34 -0300

    Front-End Done

--- web/src/Pages/CreatePoint/index.tsx
+++ web/src/Pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState, ChangeEvent} from "react";
-import {Link} from 'react-router-dom';
+import React, {useEffect, useState, ChangeEvent, FormEvent} from "react";
+import {Link, useHistory} from 'react-router-dom';
 import { FiArrowLeft } from "react-icons/fi";
 import { Map, TileLayer, Marker} from 'react-leaflet';
 import { LeafletMouseEvent } from "leaflet";
@@ -44,6 +44,8 @@ const CreatePoint = () => {
     const [selectedCity, setSelectedCity] = useState('0');
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+
+    const history = useHistory();
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(position => {
@@ -113,7 +115,42 @@ const CreatePoint = () => {
     }
 
     function handleSelectItem(id: number) {
+        const alreadySelected = selectedItems.findIndex(item => item ===id);
 
+        if (alreadySelected >= 0) {
+            const filteredItems = selectedItems.filter(item => item !== id);
+
+            setSelectedItems(filteredItems);
+        } else {
+            setSelectedItems([...selectedItems, id])
+        }
+    }
+
+    async function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+
+        const {name, email, whatsapp} = formData;
+        const uf = selectedUf;
+        const city = selectedCity;
+        const [latitude, longitude] = selectedPosition;
+        const items = selectedItems;
+
+        const data = {
+            name,
+            email,
+            whatsapp,
+            uf,
+            city,
+            latitude,
+            longitude,
+            items
+        };
+
+        await api.post('points, data');
+
+        alert('Ponto de coleta criado!');
+
+        history.push('/');
     }
 
     return (
@@ -127,7 +164,7 @@ const CreatePoint = () => {
                 </Link>
             </header>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br/> ponto de coleta</h1>
 
                 <fieldset>
@@ -222,7 +259,11 @@ const CreatePoint = () => {
 
                     <ul className="items-grid">
                         {items.map(item => (
-                            <li key={item.id} onClick={() => handleSelectItem(item.id)}>
+                            <li 
+                                key={item.id} 
+                                onClick={() => handleSelectItem(item.id)}
+                                className={selectedItems.includes(item.id) ? 'selected' : ''}
+                            >
                                 <img src={item.image_url} alt={item.title} />
                                 <span>{item.title}</span>
                             </li>
